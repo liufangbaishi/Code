0f6be6914c6e89a50edcdbf83bbfbf84fd47a9ba
liufangbaishi <1728672994@qq.com>
2018-08-04 23:59:26 +0800

    引用计数

---
+++ string/string_count.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+/////////////////////////////////////引用计数////////////////////////////////////////
+class String
+{
+public:
+	String(const char* pStr = "")
+		:_count(new int(1))
+	{
+		if (NULL == pStr)
+		{
+			_pStr = new char[1];
+			*_pStr = '\0';
+		}
+		else
+		{
+			_pStr = new char[strlen(pStr) + 1];
+			strcpy(_pStr, pStr);
+		}
+	}
+	String(const String& s)
+		: _count(s._count)
+		, _pStr(s._pStr)
+	{
+		(*_count)++;
+	}
+	String& operator=(const String& s)
+	{
+		if (this != &s)
+		{
+			if (0 == --(*_count))//s3=s2
+			{
+				delete[] _pStr;
+				delete _count;
+			}
+			_pStr = s._pStr;//s2=s3
+			_count = s._count;
+			(*_count)++;
+		}
+		return *this;
+	}
+	~String()
+	{
+		if (0 == --(*_count) && _pStr)
+		{
+			delete[] _pStr;
+			_pStr = NULL;
+			delete _count;
+			_count = NULL;
+		}
+	}
+private:
+	char* _pStr;
+	int * _count;
+};
+int main()
+{
+	String s1("hello");
+	//String s2(s1);
+	//String s3;
+	//s3 = s2;
+	//s2 = s3;
+	//s3 = s1;
+	//s1 = s3;
+	return 0;
+}
